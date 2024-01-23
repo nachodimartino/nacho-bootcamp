88e42b576bb1b13419ad05afd1a51ae92f00bcaf
Ignacio Di Martino <nachojdimartino@gmail.com>
2024-01-23 09:24:34 -0300

    Primer commit

---
+++ part1/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Mensaje  from './Mensaje';
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+<Mensaje msg='Estamos trabajando '/>
+<Mensaje msg= 'En un curso'/>
+<Mensaje msg= 'De React' />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ script.js
@@ -0,0 +1,21 @@
+console.log('Hola mundo nacho')
+
+
+
+suma= (operando1,operando2) => {
+console.log(operando1)
+console.log(operando2)
+return operando1 + operando2 
+} /* function expression*/
+
+resultado= suma(2,3)
+console.log(resultado)
+
+function restar (a,b){
+
+    return a - b
+}
+
+r= restar(7,4)
+
+console.log(r)
